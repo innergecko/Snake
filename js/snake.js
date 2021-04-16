1872226e47217a02a4cf0bc88c16fa5ba2c76cd6
InnerGecko <40200114@esmad.ipp.pt>
2021-04-16 10:15:50 +0100

    repor origem da snake

--- js/snake.js
+++ js/snake.js
@@ -1,7 +1,9 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene;
-        this.scene.add.rectangle(20, 20, 16, 16, 0xff0000);
+        this.box = this.scene.add
+            .rectangle(0, 0, 16, 16, 0xff0000)
+            .setOrigin(0);
     }
 
     update(time) {
