d916f7e0d9ac77647ac7baeb1a304212653daff3
InnerGecko <40200114@esmad.ipp.pt>
2021-04-16 10:29:25 +0100

    KeyCode setas
    37 left
    38 up
    39 right
    40 down

--- js/snake.js
+++ js/snake.js
@@ -1,11 +1,38 @@
 export default class Snake {
     constructor(scene) {
         this.scene = scene;
-        this.box = this.scene.add
-            .rectangle(0, 0, 16, 16, 0xff0000)
-            .setOrigin(0);
+        this.body = [];
+
+        this.body.push(
+            this.box = this.scene.add
+                .rectangle(0, 0, 16, 16, 0xff0000)
+                .setOrigin(0)
+        );
+
+        this.body.push(
+            this.box = this.scene.add
+                .rectangle(0, 0, 16, 16, 0xff0000ff)
+                .setOrigin(0)
+        );
+
+        this.scene.input.keyboard.on('keydown', e => {
+            this.keydown(e);
+        });
+    }
+
+    keydown(event) {
+        if (event.keyCode == 37) { //Left
+        }
+        else if (event.keyCode == 38) { //Up
+        }
+        else if (event.keyCode == 39) { //Right
+        }
+        else if (event.keyCode == 40) { //Down
+        }
     }
 
     update(time) {
+        this.body[0].x += 1;
     }
 }
+
